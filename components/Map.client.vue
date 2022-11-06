1cb806af0fe110523aaba8fc66cf5110c4073a3b
Fabian Skarmeta <f.skarmeta@gmail.com>
2022-11-06 14:49:59 +0100

    add: encapsulate fn for painting friends

--- components/Map.client.vue
+++ components/Map.client.vue
@@ -4,7 +4,6 @@ import { Ref } from "vue";
 import useMap from "~~/composables/useMap";
 import friends from "~~/utils/friends";
 
-
 const map: Ref<L.Map> = ref();
 
 const centerIcon = L.icon({
@@ -18,56 +17,63 @@ const createRandomColor = () =>
 
 onMounted(async () => {
   if (process.client) {
-    await nextTick()
-    console.log("oli", document.getElementById('map'));
+    await nextTick();
+    console.log("oli", document.getElementById("map"));
     map.value = useMap("map", {
       center: [-33.443, -70.637],
       zoom: 13,
     });
-
-    const length = friends.length;
-    const coordinatesSum = friends.reduce(
-      (a, b) => ({ lat: a.lat + b.lat, lng: a.lng + b.lng }),
-      { lat: 0, lng: 0 }
-    );
-    const averageCoordinates: [number, number] = [
-      coordinatesSum.lat / length,
-      coordinatesSum.lng / length,
-    ];
-
-    L.marker(
-      { lat: averageCoordinates[0], lng: averageCoordinates[1] },
-      {
-        title: "Center",
-        icon: centerIcon,
-      }
-    ).addTo(map.value);
-
-    for (const { lat, lng, name } of friends) {
-      const friendCoordinates: [number, number] = [lat, lng];
-
-      const friendIcon = L.icon({
-        className: "friend-icon",
-        iconUrl:
-          "https://lh3.googleusercontent.com/a/ALm5wu2e6rbUQZqjYd1u_8F0b4BWfWNtfZ3Z-Bg5toaiOQ=s96-c",
-        iconSize: [50, 50],
-      });
-
-      const color = createRandomColor();
-      const line = L.polyline([friendCoordinates, averageCoordinates], {
-        color,
-      });
-
-      L.marker({ lat, lng }, { title: name, icon: friendIcon }).addTo(
-        map.value
-      );
-      const tooltip = L.tooltip().setContent(name);
-      line.bindTooltip(tooltip).addTo(map.value);
-    }
+    paintFriends();
   }
 });
+
+const paintFriends = () => {
+  const length = friends.length;
+  const coordinatesSum = friends.reduce(
+    (a, b) => ({ lat: a.lat + b.lat, lng: a.lng + b.lng }),
+    { lat: 0, lng: 0 }
+  );
+  const averageCoordinates: [number, number] = [
+    coordinatesSum.lat / length,
+    coordinatesSum.lng / length,
+  ];
+
+  L.marker(
+    { lat: averageCoordinates[0], lng: averageCoordinates[1] },
+    {
+      title: "Center",
+      icon: centerIcon,
+    }
+  ).addTo(map.value);
+
+  for (const { lat, lng, name } of friends) {
+    const friendCoordinates: [number, number] = [lat, lng];
+
+    const friendIcon = L.icon({
+      className: "friend-icon",
+      iconUrl:
+        "https://lh3.googleusercontent.com/a/ALm5wu2e6rbUQZqjYd1u_8F0b4BWfWNtfZ3Z-Bg5toaiOQ=s96-c",
+      iconSize: [50, 50],
+    });
+
+    const color = createRandomColor();
+    const line = L.polyline([friendCoordinates, averageCoordinates], {
+      color,
+    });
+
+    L.marker({ lat, lng }, { title: name, icon: friendIcon }).addTo(map.value);
+    const tooltip = L.tooltip().setContent(name);
+    line.bindTooltip(tooltip).addTo(map.value);
+  }
+};
+
+const test = () =>
+  map.value.eachLayer((layer) => {
+    layer.remove();
+  });
 </script>
 <template>
+  <Button @click="test">test</Button>
   <div id="map" class="h-full w-full"></div>
 </template>
 
