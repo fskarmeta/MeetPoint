f419a1b1bf24f50ce39d426735929810313172a7
Fabian Skarmeta <f.skarmeta@gmail.com>
2022-11-06 21:08:20 +0100

    add: vintage map background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import backgroundImage from "../assets/images/vintage-map.jpg";
+</script>
 
 <template>
-  <div class="w-screen h-screen">
-    <div class="mx-auto h-full flex justify-center">
+  <div class="w-screen h-screen relative overflow-hidden">
+    <img
+      class="w-screen h-screen absolute opacity-20"
+      :src="backgroundImage"
+      alt="background-image"
+    />
+
+    <div class="mx-auto h-full flex justify-center relative">
       <slot />
     </div>
   </div>
