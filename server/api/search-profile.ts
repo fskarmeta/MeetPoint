b5a37c1cad63e1fbaaf8804509e3a0f9a7bec195
Fabian Skarmeta <fabian.skarmeta@envelio.de>
2022-12-18 23:03:43 +0100

    add: exclude current user and friends from profile search

--- server/api/search-profile.ts
+++ server/api/search-profile.ts
@@ -4,17 +4,35 @@ import {
 } from "#supabase/server";
 
 export default eventHandler(async (event) => {
-  const client = serverSupabaseServiceRole(event);
   const user = await serverSupabaseUser(event);
   if (!user) {
     throw new Error("Not authorized");
   }
+  const client = serverSupabaseServiceRole(event);
   const { id: currentUserId } = user;
+
+  // get all current friends independant of status accepted or pending
+  const { data: friendsData } = await client
+    .from("profiles")
+    .select("friends(*)")
+    .eq("id", currentUserId)
+    .single();
+
+  // store all ids we want to filter out from the search results
+  const profileIdsToFilterOut = [currentUserId];
+
+  if (friendsData && friendsData.friends) {
+    friendsData.friends.forEach((friend) => {
+      profileIdsToFilterOut.push(friend.friend_id);
+    });
+  }
+
+  // get all profiles that are not the current user and not in the list of friends
   const profiles = await client
     .from("profiles")
     .select("username, id, avatar_url")
     .neq("username", null)
-    .neq("id", currentUserId);
+    .not("id", "in", `(${profileIdsToFilterOut.join(",")})`);
 
   return profiles;
 });
