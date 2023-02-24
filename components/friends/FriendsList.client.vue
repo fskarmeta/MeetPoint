cbd95afea15c631c458ec3240da461756f848678
Fabian Skarmeta <fabian.skarmeta@envelio.de>
2023-02-24 02:12:50 +0100

    add: use icons for handling users

--- components/friends/FriendsList.client.vue
+++ components/friends/FriendsList.client.vue
@@ -7,7 +7,7 @@ onMounted(async () => {
 });
 
 const deleteFriend = async (friendId: string) => {
-  userStore.handleDeleteFriend(friendId)
+  userStore.handleDeleteFriend(friendId);
   useFetch("/api/delete-friend", {
     method: "DELETE",
     body: JSON.stringify({ friendId: friendId }),
@@ -30,18 +30,52 @@ const acceptFriend = async (friendId: string) => {
       :list="userStore.invited"
       title="Sent Invitations"
     >
-      <p class="text-red-500" @click="deleteFriend(slotProps.id)">Cancel</p>
+      <ATooltip>
+        <template #title>Cancel Invitation</template>
+        <CloseOutlined
+          :style="{ color: 'red' }"
+          class="cursor-pointer"
+          @click="deleteFriend(slotProps.id)"
+        />
+      </ATooltip>
     </FriendsSingleList>
     <FriendsSingleList
       v-slot="slotProps"
       :list="userStore.pending"
       title="Pending invitation"
     >
-      <p class="text-green-500" @click="acceptFriend(slotProps.id)">Accept</p>
-      <p class="text-red-500" @click="deleteFriend(slotProps.id)">Cancel</p>
+      <ATooltip>
+        <template #title>Accept as friend</template>
+        <CheckCircleOutlined
+          :style="{ color: 'green' }"
+          class="cursor-pointer"
+          @click="acceptFriend(slotProps.id)"
+        />
+      </ATooltip>
+      <ATooltip>
+        <template #title>Reject Invitation</template>
+        <CloseOutlined
+          :style="{ color: 'red' }"
+          class="cursor-pointer"
+          @click="deleteFriend(slotProps.id)"
+        />
+        <CheckCircleOutlined />
+      </ATooltip>
     </FriendsSingleList>
-    <FriendsSingleList v-slot="slotProps" :list="userStore.friends" title="Friends">
-      <p class="text-red-500" @click="deleteFriend(slotProps.id)">Delete</p>
+    <FriendsSingleList
+      v-slot="slotProps"
+      :list="userStore.friends"
+      title="Friends"
+    >
+      <ATooltip>
+        <template #title>Finish friendship</template>
+        <CloseOutlined
+          :style="{ color: 'red' }"
+          class="cursor-pointer"
+          @click="deleteFriend(slotProps.id)"
+        />
+        <CheckCircleOutlined />
+      </ATooltip>
     </FriendsSingleList>
   </div>
 </template>
